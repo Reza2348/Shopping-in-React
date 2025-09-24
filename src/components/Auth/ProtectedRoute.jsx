0215460e2b0d317704df9c053ff4e1b28a49db81
Amier Reza majidaie <146933133+Reza2348@users.noreply.github.com>
2025-09-24 21:40:40 +0330

    Update ProtectedRoute.jsx

--- src/components/Auth/ProtectedRoute.jsx
+++ src/components/Auth/ProtectedRoute.jsx
@@ -1,8 +1,14 @@
+import React from "react";
+import { Navigate } from "react-router"; // <- حتماً باید این را اضافه کنید
+
 const ProtectedRoute = ({ children, requireAuth = true }) => {
   const isAuthenticated = Boolean(localStorage.getItem("user"));
 
   if (requireAuth && !isAuthenticated) {
     return <Navigate to="/login" replace />;
   }
+
   return children;
 };
+
+export default ProtectedRoute;
